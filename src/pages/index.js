0b1905ea213121c9aa9a20b5422b3c5fc96c9b1e
baysxz <baysgalanxz@gmail.com>
2024-08-22 07:58:56 +0800

    b

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,50 +1,48 @@
-import { Skill } from "@/Components/icons/Skill";
-import {JavascriptIcon} from "../Components/icons/JavascriptIcons";
+// import { Skill } from "@/Components/Skill";
+// import { JavascriptIcon } from "../Components/icons/JavascriptIcons";
 
-const skills = [ {
-  key:1,
-  name: JavascriptIcon,
-  icon: <JavascriptIcon/>
-},
-{
-  key:2,
-  name: JavascriptIcon,
-  icon: <JavascriptIcon/>
-},
-{
-  key:3,
-  name: JavascriptIcon,
-  icon: <JavascriptIcon/>
-},
-{
-  key:4,
-  name: JavascriptIcon,
-  icon: <JavascriptIcon/>
-},
-{
-  key:5,
-  name: JavascriptIcon,
-  icon: <JavascriptIcon/>
-},
-{
-  key:6,
-  name: JavascriptIcon,
-  icon: <JavascriptIcon/>
-},
-];
-export default function Home () {
-  return (
-    <div className="grid grid-cols-3 place-content-center">
-      {skills.map((skill)=> {
-const {id, ...other}=skill;
-return ( 
-<Skill key={id} {...other}/>
-
-);
-      })}
-    </div>
-  );
-}
+// const skills = [
+//   {
+//     id: 1,
+//     name: JavascriptIcon,
+//     icon: <JavascriptIcon />,
+//   },
+//   {
+//     id: 2,
+//     name: JavascriptIcon,
+//     icon: <JavascriptIcon />,
+//   },
+//   {
+//     id: 3,
+//     name: JavascriptIcon,
+//     icon: <JavascriptIcon />,
+//   },
+//   {
+//     id: 4,
+//     name: JavascriptIcon,
+//     icon: <JavascriptIcon />,
+//   },
+//   {
+//     id: 5,
+//     name: JavascriptIcon,
+//     icon: <JavascriptIcon />,
+//   },
+//   {
+//     id: 6,
+//     name: JavascriptIcon,
+//     icon: <JavascriptIcon />,
+//   },
+// ];
+// export default function Home() {
+//   return (
+//     <div className='grid grid-cols-3 place-content-center'>
+//       {skills.map((skill) => {
+//         const { id, ...other } = skill;
+//         return <Skill key={id} {...other} />;
+//       })}
+//     </div>
+//   );
+// }
 
 // export default function Home() {
 //   return ( <div>
@@ -54,4 +52,3 @@ return (
 //   </div>
 //   );
 // };
-
---
+++ src/pages/navbar.js
@@ -0,0 +1,26 @@
+// export default function Hero() {
+//     return (
+//       <section className="bg-gray-900 text-white h-screen flex items-center justify-center">
+//         <div className="text-center">
+//           <h1 className="text-5xl font-bold">Hello, I'm [Your Name]</h1>
+//           <p className="mt-4 text-xl">A passionate developer specializing in [Your Skills]</p>
+//           <a href="#projects" className="mt-8 inline-block bg-blue-500 px-6 py-3 rounded-lg">View My Work</a>
+//         </div>
+//       </section>
+//     );
+//   }
+
+// import { MenuIcon } from "@/Components/icons/MenuIcon";
+
+// export default function Header() {
+//   return (
+//     <div className='flex justify-between'>
+//       <h1
+//         className='font-normal text-3xl text-center"
+//       '>
+//         BAY
+//       </h1>
+//       <MenuIcon className='size-6 p-6' />
+//     </div>
+//   );
+// }
