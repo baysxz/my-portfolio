5fcea114797736a2753da0d6dc4429861535e69a
baysxz <baysgalanxz@gmail.com>
2024-08-21 12:12:27 +0800

    vercel deployment

---
+++ src/Components/icons/Skill.js
@@ -0,0 +1,10 @@
+import { JavascriptIcon } from "../icons/JavascriptIcons";
+
+export const Skill = () => {
+    return (
+        <div>
+            <JavascriptIcon/>
+            <p>Javascript</p>
+        </div>
+    );
+};
