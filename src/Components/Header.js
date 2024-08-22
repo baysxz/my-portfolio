a20a13f6118f5cf1eb5fc64d29c6cd84ee076a5c
baysxz <baysgalanxz@gmail.com>
2024-08-22 09:18:33 +0800

    a

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+
+// import { MenuIcon } from "@/Components/icons/MenuIcon";
+
+// export default function Header() {
+//   return (
+//     <div className='flex justify-between'>
+//       <h1
+//         className='font-normal text-3xl text-center"
+//       '>
+//         BAY
+//       </h1>
+//       <MenuIcon className='size-6 p-6' />
+//     </div>
+//   );
+// }
