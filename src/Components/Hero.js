f4297c7cf398fcdb95b2f8bcca95579d4d5e03f7
baysxz <baysgalanxz@gmail.com>
2024-08-22 09:29:18 +0800

    a

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,10 +1,11 @@
+import { DEV_MIDDLEWARE_MANIFEST } from "next/dist/shared/lib/constants";
+import { LocationIcon } from "./icons/LocationIcon";
+
 export default function Hero() {
     return (
         <div className="text-center">
           <h1 className="text-5xl font-bold">Hi, I’m Tom 👋</h1>
           <p>I specialize in full stack development, particularly with React.js and Node.js.
           My main goal is to create exceptional digital experiences that are fast, visually appealing, and accessible to everyone. With over 7 years of experience in web development, I continue to find joy in crafting innovative solutions and designs.</p>
-        </div>
-   
-    );
+        </div> )
   }
