1ecdd6b7f88722da97f40bda0935401743c64ea5
baysxz <baysgalanxz@gmail.com>
2024-12-10 15:00:33 +0800

    svg

--- src/Components/icons/Nextjs.js
+++ src/Components/icons/Nextjs.js
@@ -1,17 +1,16 @@
 export const Nextjs = () => {
   return (
     <svg
-      width="64"
-      height="64"
-      viewBox="0 0 64 64"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
+      width='64'
+      height='64'
+      viewBox='0 0 64 64'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'>
       <path
-        d="M32 62.9333C49.084 62.9333 62.9333 49.084 62.9333 32C62.9333 14.916 49.084 1.06665 32 1.06665C14.916 1.06665 1.06665 14.916 1.06665 32C1.06665 49.084 14.916 62.9333 32 62.9333Z"
-        fill="black"
-        stroke="white"
-        stroke-width="6"
+        d='M32 62.9333C49.084 62.9333 62.9333 49.084 62.9333 32C62.9333 14.916 49.084 1.06665 32 1.06665C14.916 1.06665 1.06665 14.916 1.06665 32C1.06665 49.084 14.916 62.9333 32 62.9333Z'
+        fill='black'
+        stroke='white'
+        strokeWidth='6'
       />
     </svg>
   );
